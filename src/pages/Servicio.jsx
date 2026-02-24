e74fea3a2b14ec38d8055de5fb70749e63ab8c5f
reyesdereyes <jesus.enrique0z05@gmail.com>
2026-02-24 00:26:21 -0400

    Actualizacion 2

--- src/pages/Servicio.jsx
+++ src/pages/Servicio.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "../css/Servicio.css";
+import "../css/servicio.css";
 
 const Servicio = () => {
   const [activeService, setActiveService] = useState(null);
